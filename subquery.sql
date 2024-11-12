c4370572111f743c23ce35fc8adcafde9957de8e
jishan1019 <imransilamjishan80@gmail.com>
2024-11-12 22:37:21 +0600

    sub query practice

---
+++ subquery.sql
@@ -0,0 +1,7 @@
+SELECT * FROM employees WHERE salary > 50000
+
+
+SELECT max(salary) FROM employees WHERE department_name = 'HR'
+
+
+SELECT * FROM employee > SELECT max(salary) FROM employees WHERE department_name = 'HR'
